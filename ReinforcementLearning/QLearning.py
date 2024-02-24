61fb6547fc8cb8ed1838be6bda495a17bcf1ab4d
Esra Ablak <esrablk9@gmail.com>
2024-02-24 14:09:31 +0300

    rl

--- ReinforcementLearning/QLearning.py
+++ ReinforcementLearning/QLearning.py
@@ -5,9 +5,13 @@ env = gym.make("MountainCar-v0")
 
 env.reset()
 
-print(env.observation_space.high)
-print(env.observation_space.low)
-print(env.action_space.n)
+LEARNING_RATE = 0.1
+DISCOUNT = 0.95
+EPISODES = 25000
+
+# print(env.observation_space.high)
+# print(env.observation_space.low)
+# print(env.action_space.n)
 
 DISCRETE_OS_SIZE = [20] * len(env.observation_space.high)
 discrete_os_win_size = (env.observation_space.high - env.observation_space.low) / DISCRETE_OS_SIZE
@@ -15,8 +19,8 @@ discrete_os_win_size = (env.observation_space.high - env.observation_space.low) 
 print(discrete_os_win_size)
 
 q_table = np.random.uniform(low=-2, high=0, size=(DISCRETE_OS_SIZE + [env.action_space.n]))
-print(q_table.shape)
-print(q_table)
+# print(q_table.shape)
+# print(q_table)
 """
 done = False
 
