efdfe6010a8b3fb870cd8df42d7d914f5cca1ef3
Esra Ablak <esrablk9@gmail.com>
2024-02-26 08:10:50 +0300

    QLearningTemplate

---
+++ ReinforcementLearning/gym_taxi_QLearning.py
@@ -0,0 +1,41 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Feb 26 08:02:12 2024
+
+@author: esraablak
+"""
+
+import gym
+import numpy as np
+import random
+import matplotlib.pyplot as plt
+
+env = gym.make("Taxi-v3").env
+
+# Q Table
+
+# Hyperparameter
+
+# Plotting Matrix
+
+episode_number = 10000
+for i in range(1,episode_number):
+    
+    # initalize environment
+    
+    while True:
+        
+        # exploit vs explore to find action
+        
+        # action process and take reward/observation
+        
+        # Q learning function
+        
+        # Q table update
+        
+        # update state
+        
+        # find wrong dropout
+        
+        if done:
+            break
