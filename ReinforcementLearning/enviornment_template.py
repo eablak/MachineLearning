493eb27157c290b2c2be8aa6476590287036bfb2
Esra Ablak <esrablk9@gmail.com>
2024-02-27 16:48:25 +0300

    brotherEWWW

--- ReinforcementLearning/enviornment_template.py
+++ ReinforcementLearning/enviornment_template.py
@@ -8,6 +8,12 @@ Created on Tue Feb 27 12:53:03 2024
 # pygame template
 import pygame
 import random
+import numpy as np
+from collections import deque
+from keras.models import Sequential
+from keras.layers import Dense
+from keras.optimizers import Adam
+import random
 
 # window size
 WIDTH = 360
@@ -34,12 +40,12 @@ class Player(pygame.sprite.Sprite):
         self.rect.bottom = HEIGHT - 1
         self.speed_x = 0
         
-    def update(self):
+    def update(self, action):
         self.speed_x = 0
         keystate = pygame.key.get_pressed()
-        if keystate[pygame.K_LEFT]:
+        if keystate[pygame.K_LEFT] or action == 0:
             self.speed_x = -4
-        elif keystate[pygame.K_RIGHT]:
+        elif keystate[pygame.K_RIGHT] or action == 1:
             self.speed_x = 4
         else:
             self.speed_x = 0
@@ -82,54 +88,200 @@ class Enemy(pygame.sprite.Sprite):
             
     def getCoordinates(self):
         return (self.rect.x, self.rect.y)
-
-# initalize pygame and create window
-pygame.init()
-screen = pygame.display.set_mode((WIDTH, HEIGHT))
-pygame.display.set_caption("RL Game")
-clock = pygame.time.Clock()
-
-# sprite
-all_sprite = pygame.sprite.Group()
-enemy = pygame.sprite.Group()
-
-player = Player()
-m1 = Enemy()
-m2 = Enemy()
-
-enemy.add(m1)
-enemy.add(m2)
-all_sprite.add(m1)
-all_sprite.add(m2)
-all_sprite.add(player)
-
-# game loop
-running = True
-while running:
-    # keep loop runing at the right speed
-    clock.tick(FPS)
     
-    # process input
-    for event in pygame.event.get():
-        if event.type == pygame.QUIT:
-            running = False
+    
+class DQLAgent:
+    def __init__(self):
+        # parameter / hyperparameter
+        self.state_size = 4 # (distance playerx-m1x),(distance playerx-m1y),(distance playerx-m2x),(distance playerx-m2y)
+        self.action_size = 3 # right, left, no move
+        
+        self.gamma = 0.95
+        self.learning_rate = 0.01
+        
+        self.epsilon = 1 # explore
+        self.epsilon_decay = 0.995
+        self.epsilon_min = 0.01
+        
+        self.memory = deque(maxlen = 1000)
+        
+        self.model = self.build_model()
+    
+    def build_model(self):
+        # neural network for deep q learning
+        model = Sequential()
+        model.add(Dense(48, input_dim=self.state_size, activation="relu"))
+        model.add(Dense(self.action_size, activation="linear"))
+        model.compile(loss="mse", optimizer=Adam(lr=self.learning_rate))
+        return model
+    
+    def remember(self, state, action, reward, next_state, done):
+        # storage
+        self.memory.append((state,action,reward,next_state,done))
+    
+    def act(self, state):
+        state = np.array(state)
+        if np.random.rand() <= self.epsilon:
+            return random.randrange(self.action_size)
+        act_values = self.model.predict(state)
+        return np.argmax(act_values[0])
+    
+    def replay(self, batch_size):
+        # training
+        if len(self.memory) < batch_size:
+            return
+        mini_batch = random.sample(self.memory, batch_size)
+        for state, action, reward, next_state, done in mini_batch:
+            state = np.array(state)
+            next_state = np.array(next_state)
+            if done:
+                target = reward
+            else:
+                target = reward + self.gamma*np.amax(self.model.predict(next_state)[0])
+            train_target = self.model.predict(state)
+            train_target[0][action] = target
+            self.model.fit(state, train_target, verbose=0)
             
-    # update
-    all_sprite.update()
-    
-    # collide
-    hits = pygame.sprite.spritecollide(player, enemy, False, pygame.sprite.collide_circle)
-    
-    if hits:
-        running = False
-        print("Game Over")
-    
-    # draw / render (show)
-    screen.fill(GREEN)
-    all_sprite.draw(screen)
-    
-    # after drawing flip display
-    pygame.display.flip()
+    def adaptiveEGreedy(self):
+        if self.epsilon > self.epsilon_min:
+            self.epsilon *= self.epsilon_decay
+            
     
     
-pygame.quit()
+class Env(pygame.sprite.Sprite):
+    def __init__(self):
+        pygame.sprite.Sprite.__init__(self)
+        self.all_sprite = pygame.sprite.Group()
+        self.enemy = pygame.sprite.Group()
+        self.player = Player()
+        self.all_sprite.add(self.player)
+        self.m1 = Enemy()
+        self.m2 = Enemy()
+        self.all_sprite.add(self.m1)
+        self.all_sprite.add(self.m2)
+        self.enemy.add(self.m1)
+        self.enemy.add(self.m2)
+        
+        self.reward = 0
+        self.total_reward = 0
+        self.done = False
+        self.agent = DQLAgent()
+        
+    def findDistance(self, a, b):
+        d = a-b
+        return d
+    
+    def step(self, action):
+        state_list = []
+        
+        # update
+        self.player.update(action)
+        self.enemy.update()
+        
+        # get coordinate
+        next_player_state = self.player.getCoordinates()
+        next_m1_state = self.m1.getCoordinates()
+        next_m2_state = self.m2.getCoordinates()
+        
+        # find distance
+        state_list.append(self.findDistance(next_player_state[0],next_m1_state[0]))
+        state_list.append(self.findDistance(next_player_state[1],next_m1_state[1]))
+        state_list.append(self.findDistance(next_player_state[0],next_m2_state[0]))
+        state_list.append(self.findDistance(next_player_state[1],next_m2_state[1]))
+        
+        return [state_list]
+         
+    # reset
+    def initialStates(self):
+        self.all_sprite = pygame.sprite.Group()
+        self.enemy = pygame.sprite.Group()
+        self.player = Player()
+        self.all_sprite.add(self.player)
+        self.m1 = Enemy()
+        self.m2 = Enemy()
+        self.all_sprite.add(self.m1)
+        self.all_sprite.add(self.m2)
+        self.enemy.add(self.m1)
+        self.enemy.add(self.m2)
+        
+        self.reward = 0
+        self.total_reward = 0
+        self.done = False
+    
+        state_list = []
+        
+        # get coordinate
+        player_state = self.player.getCoordinates()
+        m1_state = self.m1.getCoordinates()
+        m2_state = self.m2.getCoordinates()
+        
+        # find distance
+        state_list.append(self.findDistance(player_state[0],m1_state[0]))
+        state_list.append(self.findDistance(player_state[1],m1_state[1]))
+        state_list.append(self.findDistance(player_state[0],m2_state[0]))
+        state_list.append(self.findDistance(player_state[1],m2_state[1]))
+        
+        return [state_list]
+        
+    def run(self):
+        # game loop
+        state = self.initialStates()
+        running = True
+        batch_size = 24
+        while running:
+            self.reward = 2
+            # keep loop running at the right speed
+            clock.tick(FPS) 
+            # process input
+            for event in pygame.event.get():
+                if event.type == pygame.QUIT:
+                    running = False   
+            # update
+            action = self.agent.act(state)
+            next_state = self.step(action)
+            self.total_reward += self.reward 
+                
+            hits = pygame.sprite.spritecollide(self.player,self.enemy,False, pygame.sprite.collide_circle)   
+            if hits:
+                self.reward = -150
+                self.total_reward += self.reward 
+                self.done = True
+                running = False
+                print("Total reward: ",self.total_reward) 
+            
+            # storage
+            self.agent.remember(state, action,self.reward, next_state, self.done)
+            
+            # update state
+            state = next_state
+            
+            # training
+            self.agent.replay(batch_size)
+            
+            # epsilon greedy
+            self.agent.adaptiveEGreedy()
+            
+            # draw / render(show)
+            screen.fill(GREEN)
+            self.all_sprite.draw(screen)
+            # after drawing flip display
+            pygame.display.flip()
+    
+        pygame.quit()  
+
+if __name__ == "__main__":
+    env = Env()
+    liste = []
+    t = 0
+    while True:
+        t += 1
+        print("Episode: ",t)
+        liste.append(env.total_reward)
+
+        # initalize pygame and create window
+        pygame.init()
+        screen = pygame.display.set_mode((WIDTH, HEIGHT))
+        pygame.display.set_caption("RL Game")
+        clock = pygame.time.Clock()
+        
+        env.run()
