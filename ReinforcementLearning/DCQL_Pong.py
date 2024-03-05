6dafd5269370db39d5e52d3270b9d6118e5169e4
Esra Ablak <esrablk9@gmail.com>
2024-03-05 18:47:43 +0300

    DCQL

--- ReinforcementLearning/DCQL_Pong.py
+++ ReinforcementLearning/DCQL_Pong.py
@@ -16,7 +16,7 @@ GAME_HEIGHT = 400
 
 PADDLE_WIDTH = 15
 PADDLE_HEIGHT = 60
-PADDLE_BUFFER = 15 # x coordinate
+PADDLE_BUFFER = 15
 
 BALL_WIDTH = 20
 BALL_HEIGHT = 20
@@ -28,135 +28,187 @@ BALL_Y_SPEED = 2
 WHITE = (255,255,255)
 BLACK = (0,0,0)
 
-screen = pygame.display.set_mode((WINDOW_WIDTH, WINDOW_HEIGHT))
-
+screen = pygame.display.set_mode((WINDOW_WIDTH,WINDOW_HEIGHT))
 
 def drawPaddle(switch, paddleYPos):
     
     if switch == "left":
         paddle = pygame.Rect(PADDLE_BUFFER, paddleYPos, PADDLE_WIDTH, PADDLE_HEIGHT)
     elif switch == "right":
-        paddle = pygame.Rect((WINDOW_WIDTH - PADDLE_BUFFER - PADDLE_WIDTH), paddleYPos, PADDLE_WIDTH, PADDLE_HEIGHT)
+        paddle = pygame.Rect(WINDOW_WIDTH - PADDLE_BUFFER - PADDLE_WIDTH, paddleYPos, PADDLE_WIDTH, PADDLE_HEIGHT)
         
     pygame.draw.rect(screen, WHITE, paddle)
     
+    
 def drawBall(ballXPos, ballYPos):
+    
     ball = pygame.Rect(ballXPos, ballYPos, BALL_WIDTH, BALL_HEIGHT)
+    
     pygame.draw.rect(screen, WHITE, ball)
     
+
 def updatePaddle(switch, action, paddleYPos, ballYPos):
-    dft = 7.5
+    dft = 7.5 
+    
     # AGENT
-    if switch == "left": 
-        if action == 1:
+    if switch == "left":
+        if action == 1: # yukarı gidicek
             paddleYPos = paddleYPos - PADDLE_SPEED*dft
-        if action == 2:
+        if action == 2: # aşağı gidicek
             paddleYPos = paddleYPos + PADDLE_SPEED*dft
-        # ekrandan çıkma
-        if paddleYPos < 0:
+        
+        # ekrandan çıkmasın
+        if paddleYPos < 0: 
             paddleYPos = 0
         if paddleYPos > GAME_HEIGHT - PADDLE_HEIGHT:
-            paddleYPos = GAME_HEIGHT - PADDLE_HEIGHT        
-    elif switch == "right": # topun y'si ile paddel y'yi bir tut (paddel'in merkezi topun merkezini takip eder)
+            paddleYPos = GAME_HEIGHT - PADDLE_HEIGHT
+    elif switch == "right":
+        # amaç topun y'si ile padel'in y'sini bir tutmak
         if paddleYPos + PADDLE_HEIGHT/2 < ballYPos + BALL_HEIGHT/2:
             paddleYPos = paddleYPos + PADDLE_SPEED*dft
         if paddleYPos + PADDLE_HEIGHT/2 > ballYPos + BALL_HEIGHT/2:
-            paddleYPos = paddleYPos - PADDLE_SPEED*dft
-        # ekrandan çıkma
+            paddleYPos = paddleYPos - PADDLE_SPEED*dft   
+            
         if paddleYPos < 0:
             paddleYPos = 0
         if paddleYPos > GAME_HEIGHT - PADDLE_HEIGHT:
-            paddleYPos = GAME_HEIGHT - PADDLE_HEIGHT  
+            paddleYPos = GAME_HEIGHT - PADDLE_HEIGHT
+    
     return paddleYPos
+    
 
-
-def updateBall(paddle1YPos, paddle2YPos, ballXPos, ballYPos, ballXDirection, ballYDirection, DeltaFrameTime):
+def updateBall(paddle1YPos, paddle2YPos, ballXPos, ballYPos, ballXDirection, ballYDirection,DeltaFrameTime):
     
     dft = 7.5
-
+    
     ballXPos = ballXPos + ballXDirection*BALL_X_SPEED*dft
     ballYPos = ballYPos + ballYDirection*BALL_Y_SPEED*dft
-
+    
     score = -0.05
-    # top ekran dışına çıkmasın / top çarpınca dönsün
-    # agent
-    if (ballXPos <= PADDLE_BUFFER + PADDLE_WIDTH) and (ballYPos + BALL_HEIGHT >= paddle1YPos) and (ballYPos <= paddle1YPos + PADDLE_HEIGHT) and (ballXDirection == -1):
-        ballXDirection = 1
+    
+    # agent (agent'e çarptıysan top ona göre dönücek ve agent topu karşılayabilmiştir demek ona göre de score)
+    if (ballXPos <= (PADDLE_BUFFER + PADDLE_WIDTH)) and ((ballYPos + BALL_HEIGHT) >= paddle1YPos) and (ballYPos <= (paddle1YPos + PADDLE_HEIGHT)) and (ballXDirection == -1):
+        
+        ballXDirection = 1 
+        
         score = 10
+    # agent topu karşılayamadı
     elif (ballXPos <= 0):
+        
         ballXDirection = 1
-        score = -10   
-        return [score, ballXPos, ballYPos, ballXDirection, ballYDirection]
-    
-    
-    if (ballXPos >= WINDOW_WIDTH - PADDLE_WIDTH - PADDLE_BUFFER) and (ballYPos + BALL_HEIGHT >= paddle2YPos) and (ballYPos <= paddle2YPos + PADDLE_HEIGHT) and (ballXDirection == 1):
+        
+        score = -10 
+        
+        return [score, ballXPos ,ballYPos ,ballXDirection, ballYDirection]
+    # diğer paddle
+    if ((ballXPos >= (WINDOW_WIDTH - PADDLE_WIDTH - PADDLE_BUFFER)) and ((ballYPos + BALL_HEIGHT)>= paddle2YPos) and (ballYPos <= (paddle2YPos + PADDLE_HEIGHT)) and (ballXDirection == 1)):
+        
         ballXDirection = -1
-    elif ballXPos >= WINDOW_WIDTH - BALL_WIDTH:
-        ballXDirection = -1
-        return [score, ballXPos, ballYPos, ballXDirection, ballYDirection]
     
+    elif(ballXPos >= WINDOW_WIDTH - BALL_WIDTH):
+        
+        ballXDirection = -1
+        
+        return [score, ballXPos,ballYPos, ballXDirection, ballYDirection]
+    
+    # yukarıya veya aşağıya çarparsa topun yönünü ona göre değiştir
     if ballYPos <= 0:
+        
         ballYPos = 0
+        
         ballYDirection = 1
+        
     elif ballYPos >= GAME_HEIGHT - BALL_HEIGHT:
+        
         ballYPos = GAME_HEIGHT - BALL_HEIGHT
+        
         ballYDirection = -1
-        return [score, ballXPos, ballYPos, ballXDirection, ballYDirection]
-
+        
+    return [score, ballXPos,ballYPos,ballXDirection,ballYDirection]
+    
+    
 
 class PongGame:
+    
     def __init__(self):
         pygame.init()
         pygame.display.set_caption("Pong DCQL Env")
         
         self.paddle1YPos = GAME_HEIGHT/2 - PADDLE_HEIGHT/2
         self.paddle2YPos = GAME_HEIGHT/2 - PADDLE_HEIGHT/2
-               
-        self.ballXPos = WINDOW_WIDTH / 2
+        
+        self.ballXPos = WINDOW_WIDTH/2
         
         self.clock = pygame.time.Clock()
         
         self.GScore = 0.0
         
-        self.ballXDirection = random.sample([-1,1], 1)[0]
-        self.ballYDirection = random.sample([-1,1], 1)[0]
+        self.ballXDirection = random.sample([-1,1],1)[0]
+        self.ballYDirection = random.sample([-1,1],1)[0]
         
         self.ballYPos = random.randint(0,9)*(WINDOW_HEIGHT - BALL_HEIGHT)/9
         
-    def InitialDispilay(self):
+        
+    def InitialDisplay(self):
         
         pygame.event.pump()
         
         screen.fill(BLACK)
         
         drawPaddle("left", self.paddle1YPos)
-        drawPaddle("right", self.paddle2YPos)
+        drawPaddle("right",self.paddle2YPos)
+        
         drawBall(self.ballXPos, self.ballYPos)
-    
+        
         pygame.display.flip()
         
-    def playNextMove(self, action):
+        
+    def PlayNextMove(self, action):
         
         DeltaFrameTime = self.clock.tick(FPS)
         
         pygame.event.pump()
+        
         score = 0
+        
         screen.fill(BLACK)
         
         self.paddle1YPos = updatePaddle("left", action, self.paddle1YPos, self.ballYPos)
         drawPaddle("left", self.paddle1YPos)
-        
+
         self.paddle2YPos = updatePaddle("right", action, self.paddle2YPos, self.ballYPos)
         drawPaddle("right", self.paddle2YPos)
         
-        [score, self.ballXPos, self.ballYPos, self.ballXDirection, self.ballYDirection] = updateBall(self.paddle1YPos, self.paddle2YPos, self.ballXPos, self.ballYPos, self.ballXDirection, self.ballYDirection, DeltaFrameTime)
+        [score, self.ballXPos, self.ballYPos, self.ballXDirection, self.ballYDirection] = updateBall(self.paddle1YPos, self.paddle2YPos, self.ballXPos, self.ballYPos, self.ballXDirection, self.ballYDirection,DeltaFrameTime)
         
         drawBall(self.ballXPos, self.ballYPos)
-
-        if score > 0.5 or score < -0.5:
-            self.GScore = self.GScore*0.9 + 0.1*score
-
+        
+        if ( score > 0.5 or score < -0.5):
+            self.GScore = self.GScore*0.9 + 0.1*score 
+            
+        # cnn için oyunun resmini alıyoruz
         ScreenImage = pygame.surfarray.array3d(pygame.display.get_surface())
+        
         pygame.display.flip()
         
+        # önemli olan harekete göre sonuçlanan skor ve screenimage
         return [score, ScreenImage]
+    
+        
+"""
+Pong game 3 kisimda yazilir
+1) ENV
+2) Action
+3) Bu ikisini çaliştiran kisim
+    
+    
+Burada önce pong game için env oluşturuyoruz. 
+init ile gerekli parametrelerimizi veriyoruz. init display ile paddle ve ball'u başlamasi 
+gereken yeri bulup draw methodlari ile onlari yerleştiriyoruz. playNextMove ile paddle ve 
+ball için update işlemleri gerçekleşiyor paddle için yukari aşaği ve sabit bunun return
+ değeri olarak tabi yeni konumu. update ball için topun çarptiği yere göre yeni konumu döner. 
+ aldiğimiz bu değerlerle tabikide sirasiyla draw ederiz. gelen değer içerisinde score olur ve
+   oyunun score değerini güncelleriz . oyunu anlayabilmek için resimlere ihtiyaç var çünkü 
+   cnn olacak o yüzden screenimage tutariz ve next move ile gerçekleşen action'a göre score
+     ve image'leri döneriz.
+"""
--- ReinforcementLearning/DCQL_TrainAgent.py
+++ ReinforcementLearning/DCQL_TrainAgent.py
@@ -5,29 +5,31 @@ Created on Wed Feb 28 15:07:18 2024
 @author: esraablak
 """
 
-import DCQL_Agent
-import DCQL_Pong
+import sys
+sys.path.append("C:/Users/ESRA  ABLAK/Desktop/pongGame")
 
+import DCQL_Pong
+import DCQL_Agent
 import numpy as np
 import skimage as skimage
 import warnings
 warnings.filterwarnings("ignore")
-import matplotlib .pyplot as plt
 
-TOTALTrainTime = 100000
+TOTAL_TrainTime = 100000
 
 IMGHEIGHT = 40
 IMGWIDTH = 40
 IMGHISTORY = 4
 
-def ProcessGameImage(RawImage):
+def ProcessGameImage(RawImage): # 420'e 400 olan resimleri 40'a 40'a çevir sonra da normalize et.
     
-    GreyImage = skimage.color.rgb2gray(RawImage)
+    GreyImage = skimage.color.rgb2gray(RawImage) # renkliden siyah-beyaza çevir
     
     CroppedImage = GreyImage[0:400,0:400]
     
-    ReducedImage = skimage.transform.resize(CroppedImage, (IMGHEIGHT, IMGWIDTH))
-    ReducedImage = skimage.exposure.rescale_intensity(ReducedImage, out_range=(0,255))
+    ReducedImage = skimage.transform.resize(CroppedImage,(IMGHEIGHT,IMGWIDTH)) # boyut azalt
+    
+    ReducedImage = skimage.exposure.rescale_intensity(ReducedImage, out_range = (0,255)) # yeniden ölçeklendir
     
     ReducedImage = ReducedImage / 128
     
@@ -35,37 +37,43 @@ def ProcessGameImage(RawImage):
 
 def TrainExperiment():
     
-    
     TrainHistory = []
     
     TheGame = DCQL_Pong.PongGame()
-    TheGame.InitialDispilay()
+    
+    TheGame.InitialDisplay()
     
     TheAgent = DCQL_Agent.Agent()
+    
     BestAction = 0
     
-    [initialScore, initialScreenImage] = TheGame.playNextMove(BestAction)
-    initialScreenImage = ProcessGameImage(initialScreenImage)
+    [InitialScore, InitialScreenImage] = TheGame.PlayNextMove(BestAction)
+    InitialGameImage = ProcessGameImage(InitialScreenImage) # gelen resmi boyutlandır
     
-    GameState = np.stack((initialScreenImage,initialScreenImage,initialScreenImage,initialScreenImage), axis=2)
+    GameState = np.stack((InitialGameImage,InitialGameImage,InitialGameImage,InitialGameImage),axis = 2) # resimleri 4'lü aldığın için birleştir
+    # biz 40x40x4 elde ettik ama keras 1x40x40x4 istiyor
     GameState = GameState.reshape(1, GameState.shape[0],GameState.shape[1],GameState.shape[2])
     
-    for i in range(TOTALTrainTime):
-        BestAction = TheAgent.findBestAction(GameState)
-        [returnScore, newScreenImage] = TheGame.playNextMove(BestAction)
+    for i in range(TOTAL_TrainTime):
         
-        newGameImage = ProcessGameImage(newScreenImage)
-        newGameImage = newGameImage.reshape(1, newGameImage.shape[0],newGameImage.shape[1],1)
+        BestAction = TheAgent.FindBestAct(GameState)
+        [ReturnScore, NewScreenImage] = TheGame.PlayNextMove(BestAction)
         
-        nextState = np.append(newGameImage, GameState[:,:,:,:3], axis=3)
+        NewGameImage = ProcessGameImage(NewScreenImage)
         
-        TheAgent.CaptureSample((GameState, BestAction, returnScore, nextState))
+        NewGameImage = NewGameImage.reshape(1,NewGameImage.shape[0],NewGameImage.shape[1],1)
+        
+        NextState = np.append(NewGameImage, GameState[:,:,:,:3], axis = 3) # 4'lü resim olacak ya 1x40x40x4 olanın 4'lük olan kısımından son 3'ünü al, axis 3 demek :,:,:,:3 yani 3.'sünde birleştirme yap demek
+        
+        TheAgent.CaptureSample((GameState,BestAction,ReturnScore,NextState)) # depola
+        
         TheAgent.Process()
         
-        GameState = nextState
+        GameState = NextState
         
         if i % 250 == 0:
-            print("train time: ", i, "game score: ", TheGame.GScore)
+            print("Train time: ",i, " game score: ",TheGame.GScore)
             TrainHistory.append(TheGame.GScore)
             
-TrainExperiment()
+        
+TrainExperiment() 
