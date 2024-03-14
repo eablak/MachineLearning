115f639184718c4080601fe7e2e72f0e4e7e368f
Esra Ablak <esrablk9@gmail.com>
2024-03-14 13:11:36 +0300

    q-learning

--- ReinforcementLearning/Q-Learning/frozenLakeQLearning.py
+++ ReinforcementLearning/Q-Learning/frozenLakeQLearning.py
@@ -5,7 +5,6 @@ Created on Mon Feb 26 08:57:42 2024
 @author: esraablak
 """
 
-
 import gym
 import numpy as np
 import random
@@ -23,7 +22,7 @@ register(
     reward_threshold=0.78, # optimum = .8196
     )
 
-# Q Table
+# Q-Table
 q_table = np.zeros([env.observation_space.n, env.action_space.n])
 
 # Hyperparameter
@@ -31,43 +30,52 @@ alpha = 0.8
 gamma = 0.95
 epsilon = 0.1
 
-# Plotting Matrix
+# Plotting Metrix
 reward_list = []
 
-episode_number = 2000
-for i in range(1,episode_number):
+episode_number = 15000 # ajan kaç kez eğitilecek
 
+for i in range(1,episode_number):
+    
     # initalize environment
     state = env.reset()
+    state = state[0]
     
     reward_count = 0
+    # ajanı 1 episode için eğitmeye başla (yani ajan yanlış yerde müşteriyi bırakana kadar)
     while True:
-
-        # exploit vs explore to find action
-        if random.uniform(0, 1) < epsilon:
+        
+        # expliot vs explore to find action -> epsilon
+        if random.uniform(0,1) < epsilon:
             action = env.action_space.sample()
         else:
             action = np.argmax(q_table[state])
-            
-        # action process and take reward/observation
-        next_state, reward, done, _ = env.step(action)
         
-        # Q learning function
+        
+        # action process and take reward / observation
+        next_state, reward, done, info, _ = env.step(action)
+        
+        
+        # q-learning function
         old_value = q_table[state,action]
         next_max = np.max(q_table[next_state])
         
         next_value = (1-alpha)*old_value + alpha*(reward + gamma*next_max)
-        # Q table update
+        
+        # update q-table
         q_table[state,action] = next_value
-
+        
         # update state
         state = next_state
-
-        reward_count+=reward
+        
+        
+        reward_count += reward
+        
         if done:
             break
-    if i%10==0:
+        
+    if i % 10 == 0:
         reward_list.append(reward_count)
-        print("Episode {}, reward {}".format(i,reward_count))
+        print("Episode {}, reward: {}".format(i,reward_count))
         
 plt.plot(reward_list)
