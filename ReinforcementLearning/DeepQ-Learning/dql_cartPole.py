507f632f51603fa3406b17402a22781e504b8e28
Esra Ablak <esrablk9@gmail.com>
2024-03-15 15:50:14 +0300

    deep-q-learning

---
+++ ReinforcementLearning/DeepQ-Learning/dql_cartPole.py
@@ -0,0 +1,160 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Feb 26 13:18:03 2024
+
+@author: esraablak
+"""
+
+import gym
+import numpy as np
+from collections import deque
+from keras.models import Sequential
+from keras.layers import Dense
+from keras.optimizers import Adam
+import random
+
+class DQLAgent:
+    
+    def __init__(self, env):
+        # hyperparameter
+        
+        # build model
+        self.state_size = env.observation_space.shape[0] # nn input layer !
+        self.action_size = env.action_space.n # nn output layer !
+        
+        # replay
+        self.gamma = 0.95 # future reward
+        self.learning_rate = 0.01
+        
+        # adaptiveEGreedy
+        self.epsilon = 1 # explore
+        self.epsilon_decay = 0.995 # her iterasyonda epsilon_decay kadar explore'u azalt (exploitation'a kaymaya başla)
+        self.epsilon_min = 0.01 # explore için min / treshold
+        
+        # remember
+        self.memory = deque(maxlen=1000) # dolduğunda FIFO
+        
+        self.model = self.build_model() # agent / nn
+    
+    def build_model(self):
+        
+        # neural network for deep q learning
+        
+        model = Sequential()
+        model.add(Dense(48, input_dim=self.state_size, activation="tanh")) # hidden layer
+        model.add(Dense(self.action_size, activation="linear")) # output layer
+        # loss / optimizer
+        model.compile(loss="mse", optimizer=Adam(learning_rate=self.learning_rate))
+        
+        return model
+    
+    def remember(self, state, action, reward, next_state, done):
+        # storage
+        self.memory.append((state, action, reward, next_state, done))
+    
+    def act(self, state):
+        # acting: explore or exploit
+        if random.uniform(0,1) <= self.epsilon:
+            return env.action_space.sample()
+        else:
+            act_values = self.model.predict(state)
+            return np.argmax(act_values[0])
+    
+    def replay(self, batch_size):
+        # training / backward update / learning
+        
+        if len(self.memory) < batch_size:
+            return
+        
+        minibatch = random.sample(self.memory, batch_size)
+        for (state, action, reward, next_state, done) in minibatch:
+            if done: # oyun bittiği (env'daki başarısızlık durumu True) için  s' yok, alabileceğin tek şey reward
+                target = reward
+            else: # 
+                # amax => birden fazla liste yan yana olduğu için önce hepsini birleştirir (flatted) sonra max'ı bulur indexini verir
+                target = reward + self.gamma*np.amax(self.model.predict(next_state)[0])
+            train_target = self.model.predict(state)
+            train_target[0][action] = target # loss
+            self.model.fit(state, train_target, verbose=0)
+            
+    
+    def adaptiveEGreedy(self):
+        if self.epsilon > self.epsilon_min:
+            self.epsilon *= self.epsilon_decay
+    
+
+if __name__ == "__main__":
+    
+    # initalize environment and agent
+    env = gym.make("CartPole-v1")
+    agent = DQLAgent(env)
+    
+    batch_size = 16
+    episodes = 100
+    for episode in range(episodes):
+        
+        # initalize environment
+        state = env.reset()
+        state = state[0]
+        state = np.reshape(state,[1,4])
+        
+        time = 0 # living penalty
+        while True:
+            
+            # action
+            action = agent.act(state) # select and action
+            
+            # process / step
+            next_state, reward, done, info, _ = env.step(action)
+            next_state = np.reshape(next_state, [1,4])
+            
+            
+            # return değerleri depola (s,a,s',r) / remember / storage
+            agent.remember(state, action, reward, next_state, done)
+            
+            # update state
+            state = next_state
+            
+            # replay / geçmiş tecrübelerini kullan
+            agent.replay(batch_size)
+            
+            # adjust epsilon
+            agent.adaptiveEGreedy()
+            
+            
+            time += 1
+            
+            if done:
+                print("Episode: {}, time: {}".format(episode,time))
+                break
+            
+# %% eğitilmiş modelini test et
+
+import time
+
+trained_model = agent
+state = env.reset() # hiç görmediği yeni env aç
+state = state[0]
+state = np.reshape(state, [1,4])
+time_t = 0
+
+while True:
+    
+    env.render() # env'i göster
+    
+    action = trained_model.act(state)
+    
+    next_state, reward, done, info, _ = env.step(action)
+    next_state = np.reshape(next_state, [1,4])
+    
+    state = next_state
+    
+    time_t += 1
+    
+    print(time_t)
+    
+    time.sleep(0.4)
+    
+    if done:
+        break
+print("Done")
--- ReinforcementLearning/DeepQ-Learning/dql_lunarLander.py
+++ ReinforcementLearning/DeepQ-Learning/dql_lunarLander.py
@@ -4,7 +4,6 @@ Created on Mon Feb 26 20:58:24 2024
 
 @author: esraablak
 """
-
 import gym
 import numpy as np
 from collections import deque
@@ -12,44 +11,43 @@ from keras.models import Sequential
 from keras.layers import Dense
 from keras.optimizers import Adam
 import random
-from collections import deque
 
-class DQLAgent:
-    def __init__(self, env):
-        # parameter / hyperparameter
+class DCQAgent:
+    
+    def __init__(self):
+        
         self.state_size = env.observation_space.shape[0]
         self.action_size = env.action_space.n
         
-        self.epsilon= 1
+        self.epsilon = 1
+        self.epsilon_decay = 0.9903
         self.epsilon_min = 0.01
-        self.epsilon_decay = 0.9993
+        
         self.gamma = 0.99
         self.learning_rate = 0.0001
         
         self.memory = deque(maxlen=4000)
         self.model = self.build_model()
         self.target_model = self.build_model()
-
+    
     def build_model(self):
-        # neural network for deep q learning
+        
         model = Sequential()
-        model.add(Dense(64, input_dim=self.state_size, activation="relu"))
+        model.add(Dense(64, input_dim = self.state_size, activation="relu"))
         model.add(Dense(64, activation="relu"))
         model.add(Dense(self.action_size, activation="linear"))
-        model.compile(loss="mse", optimizer=Adam(lr=self.learning_rate))
+        model.compile(loss="mse", optimizer=Adam(learning_rate=self.learning_rate))
         return model
-
+    
     def remember(self, state, action, reward, next_state, done):
-        # storage
         self.memory.append((state, action, reward, next_state, done))
-
+    
     def act(self, state):
-        # acting
-        if np.random.rand() <= self.epsilon:
+        if random.uniform(0,1) < self.epsilon:
             return np.random.choice(self.action_size)
         act_values = self.model.predict(state)
         return np.argmax(act_values[0])
-
+    
     def replay(self, batch_size):
         # vectorized replay method
         if len(agent.memory) < batch_size:
@@ -58,56 +56,57 @@ class DQLAgent:
         # vectorized method for experience replay
         minibatch = random.sample(self.memory, batch_size)
         minibatch = np.array(minibatch)
-        not_done_indices = np.where(minibatch[:,4] == False)
-        y = np.copy(minibatch[:,2])
-
-        # If minibatch contains any non-terminal states, use seperate update rule for those states
+        not_done_indices = np.where(minibatch[:,4] == False) # oyunun bitmediği örnekler
+        y = np.copy(minibatch[:,2]) # reward'lar
+        
         if len(not_done_indices[0] > 0):
             predict_sprime = self.model.predict(np.vstact(minibatch[:,3]))
             predict_sprime_target = self.target_model.predict(np.vstack(minibatch[:,3]))
             
-            # Non-terminal update rule
+            # ---
             y[not_done_indices] += np.multiply(self.gamma, predict_sprime_target[not_done_indices, np.argmax(predict_sprime[not_done_indices,:][0], axis=1)][0])
             
         actions = np.array(minibatch[:,1],dtype=int)
         y_target = self.nodel.predict(np.vstack(minibatch[:,0]))
         y_target[range(batch_size),actions] = y
         self.model.fit(np.vstack(minibatch[:,0]), y_target, epochs=1, verbose=0)
-
+        
     def adaptiveEGreedy(self):
         if self.epsilon > self.epsilon_min:
-           self.epsilon *= self.epsilon_decay
-           
+            self.epsilon *= self.epsilon_decay
+    
     def targetModelUpdate(self):
         self.target_model.set_weights(self.model.get_weights())
-
+    
+    
 if __name__ == "__main__":
+    pass
 
-    # initilaze env and agent
+    # initilaze gym env and agent
     env = gym.make("LunarLander-v2")
-    
-    agent = DQLAgent(env)
+    agent = DCQAgent(env)
     state_number = env.observation_space.shape[0]
-    
-    batch_size = 32
-    episodes = 1000
-    for episode in range(episodes):
 
+    batch_size = 32
+    episodes = 10000
+    for episode in episodes:
+            
         # initilaze environment
-        state = env.render()
+        state = env.reset()
+        state = state[0]
         state = np.reshape(state, [1,state_number])
         
         total_reward = 0
-        while True:
-            env.render()
+        for time in range(1000):
+            
             # act
             action = agent.act(state)
             
             # step
-            next_state, reward, done, _ = env.step(action)
+            next_state, reward, done, info, _ = env.step(action)
             next_state = np.reshape(next_state, [1, state_number])
             
-            # remember
+            # remember / storage
             agent.remember(state, action, reward, next_state, done)
             
             # update state
@@ -115,27 +114,13 @@ if __name__ == "__main__":
             
             # replay
             agent.replay(batch_size)
-            total_reward+=reward
             
-            
+            total_reward += reward
             if done:
                 agent.targetModelUpdate()
+                " her iterasyonda modeli update etmek yerine sadece bölüm sonunda update et. bir süre sonra öğrenmeye başladığında bölüm daha erken bitiyor öğrenme öğrene / üzerine koyarak gidiyor"
                 break
+            
+        # epsilon greedy
         agent.adaptiveEGreedy()
-        print("Episode: {}, Reward: {}".format(episode, total_reward))
-        
-# %% test
-import time
-trained_model = agent
-state = env.reset()
-state = np.reshape(state, [1, env.observation_space.shape[0]])
-while True:
-    env.render()
-    action = trained_model.act(state)
-    next_state, reward, done, _ = env.step(action)
-    next_state = np.reshape(next_state, [1,env.observation_space.shape[0]])
-    state = next_state
-#    time.sleep(0.8)
-    if done:
-        break
-print("Done")
+        print("Episode: {}, Reward: {}".format(episode, reward))
